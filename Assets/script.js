34598c1cc0d8ba5703d9cd5a898af31199a67191
Nick Edwards <theadventuresofnickedwards@gmail.com>
2021-12-03 11:44:05 -0800

    Updated from table data to divs for better formatting.

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,18 @@
-console.log ("Hello!")
+console.log ("Hello World!")
 
 var timeBlocks = $('#timeblocks');
 var currentHour;
 
 var currentDay = $('#currentDay');
 
+
+
+
+// Display current day and time
 function displayDayTime() {
     setInterval(function() {
         currentDay = moment ();
-        $('#currentDay').text(currentDay.format("dddd, MMMM Do, YYYY h:mm:ss a"));
+        $('#currentDay').text(currentDay.format("[Right now it's:] dddd, MMMM Do, YYYY h:mm:ss a"));
     }, 1000);
 }
 
@@ -18,11 +22,53 @@ displayDayTime();
 
 
 
+// Create hour of day, text area for tasks, and save button (adjusts for AM/PM)
+function createPlanner () {
+    for (i = 9; i < 18; i++) {
 
+        // if (i > 12) {
+        //     var hourRow = `
+        //         <tr id="${"hour-"+i}" class= "row time-block">
+        //         <td><div class="hour">${i-12 + " PM"}</div></td>
+        //         <td><textarea name="" id="" class="description"></textarea></td>
+        //         <td><button type="button" class="btn btn-primary saveBtn">💾</button></td>
+        //         </tr>`
 
-for (i = 9; i < 18; i++) {
+        //     $('#timeblocks').append(hourRow)
+        // } else {
+        //     var hourRow = `
+        //         <tr id="${"hour-"+i}" class= "row time-block">
+        //         <td><div class="hour">${i + " AM"}</div></td>
+        //         <td><textarea name="" id="" class="description"></textarea></td>
+        //         <td><button type="button" class="btn btn-primary saveBtn">💾</button></td>
+        //         </tr>`
 
+        //     $('#timeblocks').append(hourRow)
+
+        if (i > 12) {
+            var hourRow = `
+                <div id="${"hour-"+i}" class= "row time-block">
+                    <div class="hour">${i-12 + " PM"}</div>
+                    <textarea name="" id="" class="description"></textarea>
+                    <button type="button" class="btn btn-primary saveBtn">💾</button>
+                </div>`
+
+            $('#timeblocks').append(hourRow)
+        } else {
+            var hourRow = `
+                <div id="${"hour-"+i}" class= "row time-block">
+                    <div class="hour">${i + " AM"}</div>
+                    <textarea name="" id="" class="description"></textarea>
+                    <button type="button" class="btn btn-primary saveBtn">💾</button>
+                </div>`
+
+            $('#timeblocks').append(hourRow)
+
+        }
+    }
 }
+
+createPlanner();
 
 // TO DO's
 // Create timeblocks with hr, texarea, save button
